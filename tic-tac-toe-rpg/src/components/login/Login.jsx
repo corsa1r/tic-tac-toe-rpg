b14d9a57ac0bda7578fc6461abd372d8e56754da
venetak <veneta.kochovska93@gmail.com>
2025-06-22 19:12:45 +0300

    Add Login handling

--- tic-tac-toe-rpg/src/components/login/Login.jsx
+++ tic-tac-toe-rpg/src/components/login/Login.jsx
@@ -1,11 +1,38 @@
+import { useState } from 'react'
 import Menu from '../menu/Menu.jsx'
 
 function Login() {
+    const [name, setName] = useState('')
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState('')
+
+    async function SendLogin() {
+        // TODO: please do not forcefully slow it down
+        // and add proper response handling
+        const response = await fetch('/user/login', { method: 'POST', body: JSON.stringify({ name: name }), headers: { 'Content-Type': 'application/json' } })
+        setTimeout(() => {
+            setLoading('')
+            if (response.statusText) {
+                setError(response.statusText)
+                return
+            }
+        }, 2000)
+    }
+
+
+    function handleNameChange(e) {
+        setName(e.target.value);
+    }
+
     return (
         <div>
             <Menu></Menu>
             <div>Select Name</div>
-            <input type="text" name="" id="" />
+            <input onChange={handleNameChange} type="text" name="" id="" />
+            <button onClick={SendLogin}>OK</button>
+
+            <div>{loading}</div>
+            <div>{error}</div>
         </div>
     )
 }
