45de8d4e4de0cc3fbd91818194691f3645c0e335
Vladimir Enchev <vladimirenchev@Vladimirs-MacBook-Pro.local>
2025-06-22 22:28:13 +0300

    list lobbies

--- tic-tac-toe-rpg/src/components/lobbies/Lobbies.jsx
+++ tic-tac-toe-rpg/src/components/lobbies/Lobbies.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react'
+import { httpGet } from '../../helpers/http.jsx'
+import { to } from '../../helpers/await-to.jsx'
 import Menu from '../menu/Menu.jsx'
 
 function Lobbies() {
+  const [lobbies, setLobbies] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    httpGet('lobbies')
+      .then((data) => setLobbies(data))
+      .catch((err) => setError(`Error fetching lobbies: ${err.message}`))
+  }, []);
+
   return (
     <div>
       <Menu></Menu>
+      <div>{error}</div>
+      <h1>Lobbies</h1>
       <ul>
-        <li>Lobby 1</li>
-        <li>Lobby 2</li>
-        <li>Lobby 3</li>
+        {lobbies.map((lobby) => (
+          <li key={lobby.id}>
+            <h2>{lobby.name}</h2>
+            <p>Players: {lobby.players.length}</p>
+            <button disabled={lobby.players.length == 2} className="join">JOIN</button>
+          </li>
+        ))}
+        {lobbies.length === 0 && <li>No lobbies available</li>}
       </ul>
     </div>
   )
